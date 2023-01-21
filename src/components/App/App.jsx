84200c61ce719b93a67ae2f3c6f28695b123994a
Rostyslav <rossgaluzinskiy@gmail.com>
2023-01-21 11:09:47 +0200

    add

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,3 +1,4 @@
+import article from "data/article.json";
 import {
   BlogCard,
   Container,
@@ -6,16 +7,22 @@ import {
   Statistics,
   ForbesList,
   CryptoHistory,
-} from 'components';
+} from "components";
 
 export const App = () => {
   return (
     <Section>
       <Container>
-        <Heading marginBottom="50px" textAlign="center">
-          Task 1
-        </Heading>
-        <BlogCard />
+        <Heading marginBottom="50px" textAlign="center"></Heading>
+        <BlogCard
+          tag={article.tag}
+          poster={article.poster}
+          title={article.title}
+          description={article.description}
+          name={article.name}
+          avatar={article.avatar}
+          postedAt={article.postedAt}
+        />
 
         <Heading marginTop="50px" marginBottom="50px" textAlign="center">
           Task 2
